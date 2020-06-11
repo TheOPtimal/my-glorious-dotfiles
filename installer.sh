256b78b4cce55b7faedc6fd705c19770b7b5c6a4
OPtimal <41379516+TheOPtimal@users.noreply.github.com>
2020-06-11 10:19:36 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,4 +8,6 @@ echo "Installing vim-plug for neovim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Installing neovim init.vim"
 curl -fo $HOME/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/TheOPtimal/my-glorious-dotfiles/master/init.vim 
+echo "Installing plugins for neovim"
+nvim -c ":PlugInstall" -c ":q" -c ":q"
 echo "Installing finished!"
