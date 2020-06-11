1ed3dc8ddf0fd6dfdf7e1a100a010857018bc92e
OPtimal <41379516+TheOPtimal@users.noreply.github.com>
2020-06-11 09:48:25 +0300

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,9 @@
+echo "Glorious dotfiles installer/updater"
+echo ""
+echo "Installing Neovim dotfiles:"
+echo ""
+echo "Installing vim-plug for neovim"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Installing neovim init.vim"
+curl -fo $HOME/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/TheOPtimal/my-glorious-dotfiles/master/init.vim 
+echo "Installing finished!"
